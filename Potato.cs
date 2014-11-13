12ec18d1f4f8c3524049e6ee91d649e8f033ffef
Kkristjan <kristjan1_2@outlook.com>
2014-11-13 10:43:55 +0100

    Potato

---
+++ Potato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSHandler
+{
+    class Potato
+    {
+        private void latvianjoke()
+        {
+            Console.WriteLine("No potato, only sadness");
+        }
+    }
+}
